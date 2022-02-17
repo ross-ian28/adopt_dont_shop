276047109ade5f171d3a86323395dc270e56941c
Ian Ross <ianrossdrums@gmail.com>
2022-02-16 17:49:57 -0700

    feat: start user story 22

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 shelter = Shelter.find_or_create_by!(foster_program: true, name: "Ferret Shelter", city: "Denver", rank: 1)
 application = Application.find_or_create_by!(name: "Pabu", street_address: "123 Ferret St", city: "Erie", state: "Co", zipcode: "80516")
-pet = Pet.find_or_create_by!(name: "Loki", age: 1, breed: "brown", adoptable: true, shelter_id: shelter.id)
+pet1 = Pet.find_or_create_by!(name: "Loki", age: 1, breed: "brown", adoptable: true, shelter_id: shelter.id)
+pet2 = Pet.find_or_create_by!(name: "Pabu", age: 1, breed: "brown", adoptable: true, shelter_id: shelter.id)
+pet3 = Pet.find_or_create_by!(name: "Thor", age: 1, breed: "brown", adoptable: true, shelter_id: shelter.id)
